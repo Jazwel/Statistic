a31a08abe2ec184dfb401edcc297c2e604d9aad7
DS <mc_sd@bk.ru>
2023-05-11 20:40:56 -0300

    Reformat code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,7 +9,7 @@ public class StatisticsServiceTest {
 
     @Test
     void findMax() {
-        StatisticsService service = new StatisticsService ();
+        StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
@@ -18,11 +18,12 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findMax1() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 13, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected =13 ;
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
         Assertions.assertEquals(expected, actual);
