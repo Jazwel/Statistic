50df012045d33059f310cbd7c05fc4f94ff639ae
DS <mc_sd@bk.ru>
2023-06-01 11:26:49 -0300

    jacoco

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,14 +18,5 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
-    @Test
-    void findMax1() {
-        StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 13, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 13;
 
-        long actual = service.findMax(incomesInBillions);
-        Assertions.assertEquals(expected, actual);
-
-    }
 }
